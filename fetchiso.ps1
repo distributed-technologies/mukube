1a233974c1752f566ce45e9450598580d370d022
skovbakke <78202835+skovbakke@users.noreply.github.com>
2021-02-24 11:45:18 +0100

    Added comment

--- fetchiso.ps1
+++ fetchiso.ps1
@@ -1,4 +1,5 @@
 
 (Test-Path output) -or (mkdir output)
+# Note the ip is dynamic and ssh-key is stored elsewhere. This is only useable by me right now. 
 scp andreas@13.95.104.95:/home/andreas/mukube/output/rootfs.iso .\output\
 
