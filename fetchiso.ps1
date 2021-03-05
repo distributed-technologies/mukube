e51e60c093b9dd89e643f2f5f96a2be2545ea7a3
skovbakke <78202835+skovbakke@users.noreply.github.com>
2021-03-05 12:59:36 +0100

    Powershell scrips to interact with remote

--- fetchiso.ps1
+++ fetchiso.ps1
@@ -1,5 +1,8 @@
 
+# Ensure that the output folder exists
 (Test-Path output) -or (mkdir output)
 # Note the ip is dynamic and ssh-key is stored elsewhere. This is only useable by me right now. 
-scp andreas@13.95.104.95:/home/andreas/mukube/output/rootfs.iso .\output\
+scp andreas@13.95.104.95:/home/andreas/mukube/output/rootfs.iso ./output
 
+# Rerun the VM with the new iso
+Start-Process powershell -Verb runAs -ArgumentList $pwd\startVM.ps1
